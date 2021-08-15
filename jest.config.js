ddd3118645ac788b2d2429f8abe74c89f343b2cf
Maximiliano Delgado <mdelgado@baufest.com>
2021-08-15 12:15:45 -0300

    chore: edit test config

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,24 @@
 process.env.NODE_ENV = 'test';
 
+require('dotenv').config({
+  path: '.env.test',
+});
+
 module.exports = {
   moduleFileExtensions: ['js', 'json', 'ts'],
   forceExit: true,
   verbose: true,
-  rootDir: 'src',
+  rootDir: '.',
   testRegex: '.*\\.(spec|it|test|e2e)\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
+  roots: ['<rootDir>/src/'],
   collectCoverageFrom: ['**/*.(t|j)s'],
-  coverageDirectory: '../coverage',
-  coveragePathIgnorePatterns: ['node_modules', 'node_modules/*', 'mock', 'mock/*'],
+  coverageDirectory: './coverage',
+  coveragePathIgnorePatterns: ['node_modules', 'node_modules/*'],
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['jest-extended'],
+  setupFilesAfterEnv: ['./jest.setup.js', 'jest-extended'],
   moduleDirectories: ['node_modules'],
   preset: 'ts-jest',
 };
