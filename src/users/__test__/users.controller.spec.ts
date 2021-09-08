339d462cd1c6b22672a5fbfe0339f453031fbd16
Maximiliano Delgado <mdelgado@baufest.com>
2021-09-08 00:55:02 -0300

    chore: remove un used service test users

--- src/users/__test__/users.controller.spec.ts
+++ src/users/__test__/users.controller.spec.ts
@@ -7,7 +7,7 @@ import { userStub } from './stubs/user.stub';
 
 describe('UsersController', () => {
   let controller: UsersController;
-  let service: UsersService;
+  //let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -17,7 +17,7 @@ describe('UsersController', () => {
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
-    service = module.get<UsersService>(UsersService);
+    //service = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
