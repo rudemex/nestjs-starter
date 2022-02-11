52cd0b05d0e2a971700ae9e99be63081a0dfbac1
Maximiliano delgado <maximilianodelgado@MacBook-Pro-de-Maxi.local>
2022-02-11 20:26:24 -0300

    fix: an unknown value was passed to the validate function in put method

--- src/users/dtos/user.dto.ts
+++ src/users/dtos/user.dto.ts
@@ -1,5 +1,5 @@
-import { IsEmail, IsString } from '@nestjs/class-validator';
-import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { IsEmail, IsOptional, IsString } from '@nestjs/class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
   @IsString()
@@ -25,4 +25,30 @@ export class CreateUserDto {
   readonly email: string;
 }
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+//export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @IsString()
+  @ApiProperty({
+    example: 'Juan',
+    description: 'The fist name of the User',
+  })
+  @IsOptional()
+  readonly firstName: string;
+
+  @IsString()
+  @ApiProperty({
+    example: 'Perez',
+    description: 'The lastname of the User',
+  })
+  @IsOptional()
+  readonly lastName: string;
+
+  @IsString()
+  @IsEmail()
+  @ApiProperty({
+    example: 'jperez@email.com',
+    description: 'The email of the user',
+  })
+  @IsOptional()
+  readonly email: string;
+}
