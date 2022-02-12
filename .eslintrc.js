e46b581bc55fa65de3de987cd48cd73d37227b49
Maximiliano delgado <maximilianodelgado@MacBook-Pro-de-Maxi.local>
2022-02-11 22:41:39 -0300

    chore: edit character controller

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', '**/__test__/**/*'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
