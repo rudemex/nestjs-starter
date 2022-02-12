24ee68e25484a56dde29a84b8e1bf9dfe5c3a1ba
Maximiliano delgado <maximilianodelgado@MacBook-Pro-de-Maxi.local>
2022-02-12 00:25:19 -0300

    chore: add commons dependencie

--- _webpack.config.js
+++ _webpack.config.js
@@ -1,17 +1,2 @@
-const isBuild = process.env.NODE_ENV === 'build';
-module.exports = (options) => {
-  return {
-    ...options,
-    mode: isBuild ? 'production' : 'none',
-    output: {
-      ...options.output,
-      filename: '[name].js',
-      libraryTarget: 'commonjs2',
-    },
-    optimization: {
-      ...options.optimization,
-      minimize: false,
-      nodeEnv: isBuild ? 'production' : false,
-    },
-  };
-};
+const config = require('@tresdoce/nestjs-commons');
+module.exports = (options) => config.buildConfig(options);
--- jest.config.ts
+++ jest.config.ts
@@ -1,3 +1,4 @@
+import { jestConfig } from '@tresdoce/nestjs-commons';
 import * as dotenv from 'dotenv';
 
 process.env.NODE_ENV = 'test';
@@ -6,30 +7,4 @@ dotenv.config({
   path: '.env.test',
 });
 
-module.exports = {
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  forceExit: true,
-  verbose: true,
-  rootDir: '.',
-  testRegex: '.*\\.(spec|it|test|e2e|e2e-spec)\\.(t|j)s$',
-  transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
-  },
-  roots: ['<rootDir>/test/', '<rootDir>/src/'],
-  //collectCoverage: true,
-  collectCoverageFrom: ['**/*.(t|j)s', '!**/*.entity.(t|j)s'],
-  coverageDirectory: './coverage',
-  coveragePathIgnorePatterns: [
-    'node_modules',
-    'node_modules/*',
-    'dist',
-    'dist/*',
-    'src/main.ts',
-    'src/*/entities/*',
-  ],
-  testEnvironment: 'node',
-  setupFilesAfterEnv: ['./jest.setup.ts', 'jest-extended'],
-  moduleDirectories: ['node_modules'],
-  preset: 'ts-jest',
-  testResultsProcessor: 'jest-sonar-reporter',
-};
+module.exports = jestConfig;
