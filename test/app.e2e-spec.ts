495edc421aedf79b5e13c8a075212f7bfbce146c
Mex Delgado <mdelgado@tresdoce.com.ar>
2022-02-14 15:27:57 -0300

    chore: edit script to husky pre commit

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,6 +15,10 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
+  afterEach(async () => {
+    await app.close();
+  });
+
   it('/ (GET)', () => {
     return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
   });
