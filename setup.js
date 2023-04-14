b5947ecf5f0ac57fd9e9fbdac3bfaad47a2e5920
Maximiliano Delgado <mdelgado@tresdoce.com.ar>
2023-04-14 19:33:57 -0300

    test: edit docker compose and global setup jest

--- setup.js
+++ setup.js
@@ -6,8 +6,7 @@ const initDockerCompose = (
   _composeFile = 'docker-compose.yml',
 ) => {
   return async () => {
-    console.log('Init Docker Compose');
-    process.env.TESTCONTAINERS_RYUK_DISABLED = 'true';
+    console.log(`Init services from docker-compose: ${_services.join(', ')}`);
     global.__TESTCONTAINERS__ = await new DockerComposeEnvironment(
       _composeFilePath,
       _composeFile,
@@ -15,4 +14,11 @@ const initDockerCompose = (
   };
 };
 
-module.exports = initDockerCompose(['elasticsearch']);
+module.exports = initDockerCompose([
+  'elasticsearch',
+  'postgres',
+  'mysql',
+  'mongo',
+  'redis',
+  'camunda',
+]);
