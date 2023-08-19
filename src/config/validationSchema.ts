8b1241b6208ca5ef80bb63b9019ddc7698724af2
Maximiliano Delgado <mdelgado@tresdoce.com.ar>
2023-08-19 18:11:11 -0300

    feat: add propagate headers and exclude paths to healthchecks in filter

--- src/config/validationSchema.ts
+++ src/config/validationSchema.ts
@@ -1,4 +1,4 @@
-import * as Joi from 'joi';
+import Joi from 'joi';
 
 export const validationSchema = Joi.object({
   NODE_ENV: Joi.string().required(),
@@ -7,8 +7,10 @@ export const validationSchema = Joi.object({
   API_PREFIX: Joi.string().required(),
   CONTEXT: Joi.string().required(),
   ORIGINS: Joi.string().required(),
+  EXPOSED_HEADERS: Joi.string().optional(),
   ALLOWED_HEADERS: Joi.string().required(),
   ALLOWED_METHODS: Joi.string().required(),
+  PROPAGATE_HEADERS: Joi.string().optional(),
   CORS_ENABLED: Joi.boolean().required(),
   CORS_CREDENTIALS: Joi.boolean().required(),
   SWAGGER_PATH: Joi.string().required(),
