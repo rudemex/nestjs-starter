4d84bbaa6dbbfc330b7c2be4844b634944b339f7
Maximiliano Delgado <mdelgado@tresdoce.com.ar>
2024-07-28 18:43:20 -0300

    chore(envs): add function tu return env file path by NODE_ENV var

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,12 +17,12 @@ import { CharactersModule } from './characters/characters.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { config, environments, validationSchema } from './config';
+import { getEnvFilePath, config, validationSchema } from './config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: environments[`${process.env.NODE_ENV}`],
+      envFilePath: getEnvFilePath(),
       ignoreEnvFile: process.env.NODE_ENV === 'production' || false,
       load: [config],
       isGlobal: true,
