182e011c1f748706e3f7731497f1460f723ef141
Maximiliano Delgado <mdelgado@tresdoce.com.ar>
2025-04-26 21:59:41 -0300

    chore: update dependencies

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,44 +1,39 @@
+import { expect } from '@jest/globals';
+
 jest.setTimeout(30000);
 
 expect.extend({
-  toContainObject(received, argument) {
+  toContainObject(received: any[], argument: Record<string, unknown>) {
     const pass = this.equals(received, expect.arrayContaining([expect.objectContaining(argument)]));
 
-    if (pass) {
-      return {
-        message: () =>
-          `expected ${this.utils.printReceived(
-            received,
-          )} not to contain object ${this.utils.printExpected(argument)}`,
-        pass: true,
-      };
-    } else {
-      return {
-        message: () =>
-          `expected ${this.utils.printReceived(
-            received,
-          )} to contain object ${this.utils.printExpected(argument)}`,
-        pass: false,
-      };
-    }
+    return {
+      message: () =>
+        pass
+          ? `expected ${this.utils.printReceived(received)} not to contain object ${this.utils.printExpected(argument)}`
+          : `expected ${this.utils.printReceived(received)} to contain object ${this.utils.printExpected(argument)}`,
+      pass,
+    };
   },
-  toBeTypeOrNull(received, classTypeOrNull) {
-    try {
-      expect(received).toEqual(expect.any(classTypeOrNull));
-      return {
-        message: () => `Ok`,
-        pass: true,
-      };
-    } catch (error) {
-      return received === null
-        ? {
-            message: () => `Ok`,
-            pass: true,
-          }
-        : {
-            message: () => `expected ${received} to be ${classTypeOrNull} type or null`,
-            pass: false,
-          };
-    }
+
+  toBeTypeOrNull(received: any, expectedType: any) {
+    const isExpectedType = received instanceof expectedType;
+    const isNull = received === null;
+
+    return {
+      message: () =>
+        isExpectedType || isNull
+          ? `expected ${received} not to be instance of ${expectedType.name} or null`
+          : `expected ${received} to be instance of ${expectedType.name} or null`,
+      pass: isExpectedType || isNull,
+    };
   },
 });
+
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toContainObject(argument: object): R;
+      toBeTypeOrNull(expectedType: any): R;
+    }
+  }
+}
